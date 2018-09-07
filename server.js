ddbe9ad1b65465a602aab6922873794dace8b77f
Steven Mazanec <SMazanec@proexam.org>
2018-09-07 10:54:01 -0400

    fixed port error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
